cdb1d9319e3707e102d346406fea7676f18ea90a
administrator <admin@teknik.io>
2014-04-26 13:36:52 -0700

    Initial Commit

---
+++ upload.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# name: teknik-upload
+# requires: bash 4.0+, curl
+
+files="$@"
+
+if [[ -z "${files}" ]]; then
+	printf 'You must submit a file to be uploaded!\n'
+	exit 0
+else
+	printf 'Uploading file(s) now!'
+	n=1
+	for i in ${files}; do
+		printf "\nUploading file #${n} ... "
+		out=$(curl -sf -F file="@${i}" http://upload.teknik.io/lib/upload.php)
+		if [[ -n "${out}" ]]; then
+			printf "uploaded! Your file can be found at http://u.teknik.io/${out}\n"
+		else
+			printf 'error uploading file!\n'
+		fi
+		((n++))
+	done
+fi
