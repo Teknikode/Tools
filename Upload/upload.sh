5c5d8c73d90a749556de27ab8f0536bbfdba2276
Brett Bohnenkamper <kittykatt@kittykatt.us>
2014-08-13 14:59:45 -1000

    Should error out on JSON error.

--- Upload/upload.sh
+++ Upload/upload.sh
@@ -7,19 +7,20 @@ files="$@"
 
 if [[ -z "${files}" ]]; then
 	printf 'You must submit a file to be uploaded!\n'
-	exit 0
+	exit 1
 else
 	printf 'Uploading file(s) now!'
 	n=1
 	for i in "$@"; do
 		printf "\nUploading file #${n} ... "
 		out=$(curl -sf -F file="@${i}" http://api.teknik.io/upload/post)
-		if [[ -n "${out}" ]]; then
+		if [[ "${out}" =~ "error" ]]; then
+			printf 'error uploading file!\n'
+			exit 1
+		else
 			out="${out##*name\":\"}"
 			out="${out%%\"*}"
 			printf "uploaded! Your file can be found at http://u.teknik.io/${out}\n"
-		else
-			printf 'error uploading file!\n'
 		fi
 		((n++))
 	done
