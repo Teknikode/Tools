2a5954be7c3c9ef036fc7d425815d945e41b83df
Brett Bohnenkamper <kittykatt@kittykatt.us>
2014-09-11 00:38:46 -1000

    http->https

--- Upload/upload.sh
+++ Upload/upload.sh
@@ -13,14 +13,14 @@ else
 	n=1
 	for i in "$@"; do
 		printf "\nUploading file #${n} ... "
-		out=$(curl -sf -F file="@${i}" http://api.teknik.io/upload/post)
+		out=$(curl -sf -F file="@${i}" https://api.teknik.io/upload/post)
 		if [[ "${out}" =~ "error" ]]; then
 			printf 'error uploading file!\n'
 			exit 1
 		else
 			out="${out##*name\":\"}"
 			out="${out%%\"*}"
-			printf "uploaded! Your file can be found at http://u.teknik.io/${out}\n"
+			printf "uploaded! Your file can be found at https://u.teknik.io/${out}\n"
 		fi
 		((n++))
 	done
